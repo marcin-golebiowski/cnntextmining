b8434525b912554d1f3eb427a75ad82c2aafe315
xormus <xormus@c8bed19c-c04e-0410-a956-6fb8ddc5f681>
2008-06-16 20:28:57 +0000

    Experiments added

---
+++ cnntextmining/TextMining/Experiments/DefaultEvaluator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using TextMining.Model;
+
+namespace TextMining.Experiments
+{
+    class DefaultEvaluator : IEvaluator
+    {
+        private readonly TopicOriginalAssigment assigment;
+
+        public DefaultEvaluator(TopicOriginalAssigment assigment)
+        {
+            this.assigment = assigment;
+        }
+
+        public long GetScore(List<List<CNNPage>> sets)
+        {
+            long score = 0;
+
+            foreach (var set in sets)
+            {
+                for (int i = 0; i < set.Count; i++)
+                {
+                    for (int j = 0; j < set.Count; j++)
+                    {
+                        if (assigment.AreInTheSameTopic(set[i].uri, set[j].uri))
+                        {
+                            score++;
+                        }
+                    }
+                }
+            }
+            return score;
+        }
+    }
+}
---
+++ cnntextmining/TextMining/Experiments/TopicOriginalAssigment.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace TextMining.Experiments
+{
+    public class TopicOriginalAssigment
+    {
+        private readonly SqlConnection connection;
+
+        // topic url -> id of topic
+        private readonly Dictionary<string, int> topics;
+
+        // news -> a list of id's of topics
+        private readonly Dictionary<string, List<int>> assigment;
+
+        public TopicOriginalAssigment(SqlConnection connection)
+        {
+            assigment = new Dictionary<string, List<int>>();
+            topics = new Dictionary<string, int>();
+            this.connection = connection;
+        }
+
+        public void Load()
+        {
+            Console.WriteLine("Topic links loading ...");
+            assigment.Clear();
+            topics.Clear();
+
+            int topicNumbers = 0;
+
+            using (var command
+               = new SqlCommand("SELECT * FROM dbo.[Topics] WHERE LinkURL Like '%.html'", connection))
+            {
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    if (reader != null && reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+
+                            string topicURL = reader["TopicURL"].ToString();
+                            string linkURL = reader["LinkURL"].ToString();
+
+                            int topicID;
+
+                            if (!topics.ContainsKey(topicURL))
+                            {
+                                topics[topicURL] = topicNumbers;
+                                topicID = topicNumbers;
+                                topicNumbers++;
+                            }
+                            else
+                            {
+                                topicID = topics[topicURL];
+                            }
+
+
+
+                            if (assigment.ContainsKey(linkURL))
+                            {
+                                if (!assigment[linkURL].Contains(topicID))
+                                {
+                                    assigment[linkURL].Add(topicID);
+                                }
+                            }
+                            else
+                            {
+                                assigment[linkURL] = new List<int> {topicID};
+                            }
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("Topics links loaded.");
+        }
+
+
+        public bool AreInTheSameTopic(string link1, string link2)
+        {
+            if (!assigment.ContainsKey(link1)) return false;
+            if (!assigment.ContainsKey(link2)) return false;
+
+
+            foreach (int id1 in assigment[link1])
+            {
+                foreach (int id2 in assigment[link2])
+                {
+                    if (id1 == id2) return true;
+                }
+            }
+            return false;
+        }
+    }
+}
